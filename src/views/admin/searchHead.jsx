a7c762f6c0802e2d6047991b097b1a1fac0a62fa
liubiao <2427990808@qq.com>
2022-08-31 21:54:35 +0800

    优化代码

--- src/views/admin/searchHead.jsx
+++ src/views/admin/searchHead.jsx
@@ -1,143 +1,160 @@
-import { useState } from 'react'
-import { SearchOutlined, RedoOutlined } from '@ant-design/icons';
-import { Button, Form, Input, Card, DatePicker, Select, Row, Col } from 'antd';
+import { useState } from "react";
+import { SearchOutlined, RedoOutlined } from "@ant-design/icons";
+import { Button, Form, Input, Card, DatePicker, Select, Row, Col } from "antd";
 const { Option } = Select;
 const { RangePicker } = DatePicker;
-const formData = { adminName: '', phone: '', createTime: [], isUse: '', branch: '', leader: '' }
+const formData = {
+  adminName: "",
+  phone: "",
+  createTime: [],
+  isUse: "",
+  branch: "",
+  leader: "",
+};
 export default function SearchHead() {
-    const [forms] = Form.useForm()
-    const [form, setForm] = useState(formData)
-    const handleName = (e) => {
-        setForm({
-            ...form,
-            adminName: e.target.value
-        })
-    }
-    const handlePhone = (e) => {
-        setForm({
-            ...form,
-            phone: e.target.value
-        })
-    }
-    const handleCreateTime = (e, dateString) => {
-        setForm({
-            ...form,
-            createTime: dateString
-        })
-    }
-    const handleBranch = (e) => {
-        setForm({
-            ...form,
-            branch: e
-        })
-    }
-    const handleLeader = (e) => {
-        setForm({
-            ...form,
-            leader: e
-        })
-    }
-    const handleIsUse = (e) => {
-        setForm({
-            ...form,
-            isUse: e
-        })
-    }
+  const [forms] = Form.useForm();
+  const [form, setForm] = useState(formData);
+  const onReset = () => {
+    forms.resetFields();
+  };
 
-    const onReset = () => {
-        forms.resetFields()
-    }
-    return (
-        <Card>
-            <Form
-                name="basic"
-                initialValues={{
-                    remember: true,
-                }}
-                form={forms}
-                autoComplete="off"
+  const handleSearch = () => {
+    console.log(form);
+  };
+  return (
+    <Card>
+      <Form
+        name="basic"
+        initialValues={{
+          remember: true,
+        }}
+        form={forms}
+        autoComplete="off"
+      >
+        <Row gutter={24}>
+          <Col span={6}>
+            <Form.Item label="管理员姓名" name="adminName">
+              <Input
+                value={form.name}
+                onChange={(e) =>
+                  setForm({ ...form, adminName: e.target.value })
+                }
+                placeholder="请输入管理员姓名"
+              />
+            </Form.Item>
+          </Col>
+          <Col span={6}>
+            <Form.Item label="联系方式" name="phone">
+              <Input
+                value={form.phone}
+                onChange={(e) =>
+                  setForm({
+                    ...form,
+                    phone: e.target.value,
+                  })
+                }
+                placeholder="请输入联系人号码"
+              />
+            </Form.Item>
+          </Col>
+          <Col span={6}>
+            <Form.Item label="创建时间" name="createTime">
+              <RangePicker
+                value={form.searchTime}
+                onChange={(e, dateString) =>
+                  setForm({
+                    ...form,
+                    createTime: dateString,
+                  })
+                }
+                placeholder={["开始时间 ", "结束时间"]}
+              />
+            </Form.Item>
+          </Col>
+          <Col span={6}>
+            <Form.Item label="所属部门" name="branch">
+              <Select
+                value={form.branch}
+                onChange={(e) =>
+                  setForm({
+                    ...form,
+                    branch: e,
+                  })
+                }
+                placeholder="请选择所属部门"
+              >
+                <Option value="1">信息部</Option>
+                <Option value="2">运营部</Option>
+                <Option value="3">开发部</Option>
+                <Option value="4">销售部</Option>
+                <Option value="5">行政部</Option>
+                <Option value="6">财务部</Option>
+                <Option value="7">后勤部</Option>
+              </Select>
+            </Form.Item>
+          </Col>
+          <Col span={6}>
+            <Form.Item label="上级领导" name="leader">
+              <Select
+                value={form.leader}
+                onChange={(e) =>
+                  setForm({
+                    ...form,
+                    leader: e,
+                  })
+                }
+                placeholder="请选择"
+              >
+                <Option value="1">Jon</Option>
+                <Option value="2">Mike</Option>
+                <Option value="3">Boob</Option>
+                <Option value="4">Jack</Option>
+              </Select>
+            </Form.Item>
+          </Col>
+          <Col span={6}>
+            <Form.Item label="是否可用" name="isUse">
+              <Select
+                value={form.isUse}
+                onChange={(e) =>
+                  setForm({
+                    ...form,
+                    isUse: e,
+                  })
+                }
+                placeholder="请选择"
+              >
+                <Option value="1">是</Option>
+                <Option value="2">否</Option>
+              </Select>
+            </Form.Item>
+          </Col>
+        </Row>
+        <Row>
+          <Col
+            span={24}
+            style={{
+              textAlign: "right",
+            }}
+          >
+            <Button
+              onClick={handleSearch}
+              type="primary"
+              icon={<SearchOutlined />}
             >
-                <Row gutter={24}>
-                    <Col span={6}>
-                        <Form.Item
-                            label="管理员姓名"
-                            name="adminName"
-                        >
-                            <Input value={form.name} onChange={handleName} placeholder='请输入管理员姓名' />
-                        </Form.Item>
-                    </Col>
-                    <Col span={6}>
-                        <Form.Item
-                            label="联系方式"
-                            name="phone"
-                        >
-                            <Input value={form.phone} onChange={handlePhone} placeholder='请输入联系人号码' />
-                        </Form.Item>
-                    </Col>
-                    <Col span={6}>
-                        <Form.Item
-                            label="创建时间"
-                            name="createTime"
-                        >
-                            <RangePicker value={form.searchTime} onChange={handleCreateTime} placeholder={['开始时间 ', '结束时间']} />
-                        </Form.Item>
-                    </Col>
-                    <Col span={6}>
-                        <Form.Item
-                            label="所属部门"
-                            name='branch'
-                        >
-                            <Select value={form.branch} onChange={handleBranch} placeholder="请选择所属部门">
-                                <Option value="1">信息部</Option>
-                                <Option value="2">运营部</Option>
-                                <Option value="3">开发部</Option>
-                                <Option value="4">销售部</Option>
-                                <Option value="5">行政部</Option>
-                                <Option value="6">财务部</Option>
-                                <Option value="7">后勤部</Option>
-                            </Select>
-                        </Form.Item>
-                    </Col>
-                    <Col span={6}>
-                        <Form.Item
-                            label="上级领导"
-                            name="leader"
-                        >
-                            <Select value={form.leader} onChange={handleLeader} placeholder="请选择">
-                                <Option value="1">Jon</Option>
-                                <Option value="2">Mike</Option>
-                                <Option value="3">Boob</Option>
-                                <Option value="4">Jack</Option>
-                            </Select>
-                        </Form.Item>
-                    </Col>
-                    <Col span={6}>
-                        <Form.Item
-                            label="是否可用"
-                            name="isUse"
-                        >
-                            <Select value={form.isUse} onChange={handleIsUse} placeholder="请选择">
-                                <Option value="1">是</Option>
-                                <Option value="2">否</Option>
-                            </Select>
-                        </Form.Item>
-                    </Col>
-                </Row>
-                <Row>
-                    <Col span={24}
-                        style={{
-                            textAlign: 'right',
-                        }}
-                    >
-                        <Button type="primary" icon={<SearchOutlined />}>
-                            Search
-                        </Button>
-                        <Button onClick={onReset} style={{ marginLeft: 10 }} type="primary" icon={<RedoOutlined />}>
-                            Reset
-                        </Button>
-                    </Col>
-                </Row>
-            </Form>
-        </Card>
-    )
+              Search
+            </Button>
+            <Button
+              onClick={onReset}
+              style={{ marginLeft: 10 }}
+              type="primary"
+              icon={<RedoOutlined />}
+            >
+              Reset
+            </Button>
+          </Col>
+        </Row>
+      </Form>
+    </Card>
+  );
 }
--- src/views/menu/searchHead.jsx
+++ src/views/menu/searchHead.jsx
@@ -1,151 +1,169 @@
-import { useState } from 'react'
-import { SearchOutlined,RedoOutlined } from '@ant-design/icons';
-import { Button, Form, Input, Card, DatePicker, Select, Row, Col } from 'antd';
+import { useState } from "react";
+import { SearchOutlined, RedoOutlined } from "@ant-design/icons";
+import { Button, Form, Input, Card, DatePicker, Select, Row, Col } from "antd";
 const { Option } = Select;
 const { RangePicker } = DatePicker;
-const formData = { menuName: '', address: '', url: '', createTime: [], admin: '', isShow: '', isUse: '' }
+const formData = {
+  menuName: "",
+  address: "",
+  url: "",
+  createTime: [],
+  admin: "",
+  isShow: "",
+  isUse: "",
+};
 export default function SearchHead() {
-    const [forms]=Form.useForm()
-    const [form, setForm] = useState(formData)
-    const handleName = (e) => {
-        setForm({
-            ...form,
-            menuName: e.target.value
-        })
-    }
-    const handleAddress = (e) => {
-        setForm({
-            ...form,
-            address: e.target.value
-        })
-    }
-    const handleCreateTime = (e, dateString) => {
-        setForm({
-            ...form,
-            createTime: dateString
-        })
-    }
-    const handleIsShow = (e) => {
-        setForm({
-            ...form,
-            isShow: e
-        })
-    }
-    const handleAdmin = (e) => {
-        setForm({
-            ...form,
-            admin: e
-        })
-    }
-    const handleUrl = (e) => {
-        setForm({
-            ...form,
-            url: e.target.value
-        })
-    }
-    const handleIsUse = (e) => {
-        setForm({
-            ...form,
-            isUse: e
-        })
-    }
-    const onReset = () => {
-       forms.resetFields()
-    }
-    return (
-        <Card>
-            <Form
-                name="basic"
-                initialValues={{
-                    remember: true,
-                }}
-                autoComplete="off"
-                form={forms}
+  const [forms] = Form.useForm();
+  const [form, setForm] = useState(formData);
+  const onReset = () => {
+    forms.resetFields();
+  };
+
+  const handleSearch=()=>{
+    console.log(form);
+  }
+  return (
+    <Card>
+      <Form
+        name="basic"
+        initialValues={{
+          remember: true,
+        }}
+        autoComplete="off"
+        form={forms}
+      >
+        <Row gutter={24}>
+          <Col span={6}>
+            <Form.Item label="菜单名称" name="menuName">
+              <Input
+                value={form.menuName}
+                onChange={(e) =>
+                  setForm({
+                    ...form,
+                    menuName: e.target.value,
+                  })
+                }
+                placeholder="请输入菜单名称"
+              />
+            </Form.Item>
+          </Col>
+          <Col span={6}>
+            <Form.Item label="菜单地址" name="address">
+              <Input
+                value={form.address}
+                onChange={(e) =>
+                  setForm({
+                    ...form,
+                    address: e.target.value,
+                  })
+                }
+                placeholder="请输入菜单地址"
+              />
+            </Form.Item>
+          </Col>
+          <Col span={6}>
+            <Form.Item label="菜单路径" name="url">
+              <Input
+                value={form.url}
+                onChange={(e) =>
+                  setForm({
+                    ...form,
+                    url: e.target.value,
+                  })
+                }
+                placeholder="请输入菜单路径"
+              />
+            </Form.Item>
+          </Col>
+          <Col span={6}>
+            <Form.Item label="创建时间" name="createTime">
+              <RangePicker
+                value={form.createTime}
+                onChange={(e, dateString) =>
+                  setForm({
+                    ...form,
+                    createTime: dateString,
+                  })
+                }
+                placeholder={["开始时间 ", "结束时间"]}
+              />
+            </Form.Item>
+          </Col>
+          <Col span={6}>
+            <Form.Item label="创建人" name="admin">
+              <Select
+                placeholder="请选择创建人"
+                value={form.admin}
+                onChange={(e) =>
+                  setForm({
+                    ...form,
+                    admin: e,
+                  })
+                }
+              >
+                <Option value="1">Jon</Option>
+                <Option value="2">Mike</Option>
+                <Option value="3">Boob</Option>
+                <Option value="4">Jack</Option>
+              </Select>
+            </Form.Item>
+          </Col>
+          <Col span={6}>
+            <Form.Item label="是否隐藏" name="isShow">
+              <Select
+                placeholder="请选择"
+                value={form.isShow}
+                onChange={(e) =>
+                  setForm({
+                    ...form,
+                    isShow: e,
+                  })
+                }
+              >
+                <Option value="1">是</Option>
+                <Option value="2">否</Option>
+              </Select>
+            </Form.Item>
+          </Col>
+          <Col span={6}>
+            <Form.Item label="是否可用" name="isUse">
+              <Select
+                placeholder="请选择"
+                value={form.isUse}
+                onChange={(e) =>
+                  setForm({
+                    ...form,
+                    isUse: e,
+                  })
+                }
+              >
+                <Option value="1">是</Option>
+                <Option value="2">否</Option>
+              </Select>
+            </Form.Item>
+          </Col>
+        </Row>
+        <Row>
+          <Col
+            span={24}
+            style={{
+              textAlign: "right",
+            }}
+          >
+            <Button type="primary" onClick={handleSearch} icon={<SearchOutlined />}>
+              Search
+            </Button>
+            <Button
+              onClick={onReset}
+              style={{ marginLeft: 10 }}
+              type="primary"
+              icon={<RedoOutlined />}
             >
-                <Row gutter={24}>
-                    <Col span={6}>
-                        <Form.Item
-                            label="菜单名称"
-                            name="menuName"
-                        >
-                            <Input value={form.menuName} onChange={handleName} placeholder='请输入菜单名称' />
-                        </Form.Item>
-                    </Col>
-                    <Col span={6}>
-                        <Form.Item
-                            label="菜单地址"
-                            name="address"
-                        >
-                            <Input value={form.address} onChange={handleAddress} placeholder='请输入菜单地址' />
-                        </Form.Item>
-                    </Col>
-                    <Col span={6}>
-                        <Form.Item
-                            label="菜单路径"
-                            name='url'
-                        >
-                            <Input value={form.url} onChange={handleUrl} placeholder='请输入菜单路径' />
-                        </Form.Item>
-                    </Col>
-                    <Col span={6}>
-                        <Form.Item
-                            label="创建时间"
-                            name="createTime"
-                        >
-                            <RangePicker value={form.createTime} onChange={handleCreateTime} placeholder={['开始时间 ', '结束时间']} />
-                        </Form.Item>
-                    </Col>
-                    <Col span={6}>
-                        <Form.Item
-                            label="创建人"
-                            name="admin"
-                        >
-                            <Select placeholder="请选择创建人" value={form.admin} onChange={handleAdmin}>
-                                <Option value="1">Jon</Option>
-                                <Option value="2">Mike</Option>
-                                <Option value="3">Boob</Option>
-                                <Option value="4">Jack</Option>
-                            </Select>
-                        </Form.Item>
-                    </Col>
-                    <Col span={6}>
-                        <Form.Item
-                            label="是否隐藏"
-                            name="isShow"
-                        >
-                            <Select placeholder="请选择" value={form.isShow} onChange={handleIsShow}>
-                                <Option value="1">是</Option>
-                                <Option value="2">否</Option>
-                            </Select>
-                        </Form.Item>
-                    </Col>
-                    <Col span={6}>
-                        <Form.Item
-                            label="是否可用"
-                            name="isUse"
-                        >
-                            <Select placeholder="请选择" value={form.isUse} onChange={handleIsUse}>
-                                <Option value="1">是</Option>
-                                <Option value="2">否</Option>
-                            </Select>
-                        </Form.Item>
-                    </Col>
-                </Row>
-                <Row>
-                    <Col span={24}
-                        style={{
-                            textAlign: 'right',
-                        }}
-                    >
-                        <Button type="primary" icon={<SearchOutlined />}>
-                            Search
-                        </Button>
-                        <Button onClick={onReset} style={{ marginLeft: 10 }} type="primary" icon={<RedoOutlined />}>
-                            Reset
-                        </Button>
-                    </Col>
-                </Row>
-            </Form>
-        </Card>
-    )
+              Reset
+            </Button>
+          </Col>
+        </Row>
+      </Form>
+    </Card>
+  );
 }
