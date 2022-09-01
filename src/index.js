f256144c651f38ca76eb658cd97bfa07d324e966
liubiao <2427990808@qq.com>
2022-09-01 20:50:13 +0800

    新增redux

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './mock/mock'
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./mock/mock";
+import store from "./store";
+import { Provider } from "react-redux";
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-        <App />
+  <Provider store={store}>
+    <App />
+  </Provider>
 );
--- src/views/role/role.jsx
+++ src/views/role/role.jsx
@@ -1,7 +1,16 @@
-import React from 'react'
-import { Empty } from 'antd';
+import React from "react";
+import { Empty, Button } from "antd";
+import { useSelector, useDispatch } from "react-redux";
+import { add, cut } from "../../store/counterSlice";
 export default function Role() {
+  const count = useSelector((state) => state.counter.value);
+  const dispatch = useDispatch();
   return (
-    <div><Empty /></div>
-  )
+    <div>
+      <div>{count}</div>
+      <Button onClick={() => dispatch(add())}>加</Button>
+      <Button onClick={() => dispatch(cut())}>减</Button>
+      {/* <Empty /> */}
+    </div>
+  );
 }
