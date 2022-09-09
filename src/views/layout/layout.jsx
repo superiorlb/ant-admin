e9d26ebe9121f067018b6d5e8551b04c2f8b4fbb
liubiao <2427990808@qq.com>
2022-09-09 16:48:19 +0800

    修复第一次登录闪退问题

--- src/views/layout/layout.jsx
+++ src/views/layout/layout.jsx
@@ -1,27 +1,27 @@
-import { useEffect } from 'react';
-import { Outlet } from 'react-router-dom'
-import { Layout } from 'antd';
-import Sidebar from './sidebar';
-import Head from './head';
-import './layout.scss'
-import useNavigateTo from '../../hook/useNavigateTo';
-import { getToken } from '../../utils/token';
-const token = getToken()
-const { Content } = Layout
+import { useEffect } from "react";
+import { Outlet } from "react-router-dom";
+import { Layout } from "antd";
+import Sidebar from "./sidebar";
+import Head from "./head";
+import "./layout.scss";
+import useNavigateTo from "../../hook/useNavigateTo";
+import { getToken } from "../../utils/token";
+const { Content } = Layout;
 export default function Layouts() {
-    const navigateTo = useNavigateTo()
-    useEffect(() => {
-        !token && navigateTo('login')
-    })
-    return (
-        <Layout>
-            <Sidebar />
-            <Layout>
-                <Head />
-                <Content>
-                    <Outlet />
-                </Content>
-            </Layout>
-        </Layout>
-    )
+  const navigateTo = useNavigateTo();
+  useEffect(() => {
+    const token = getToken();
+    !token && navigateTo("login");
+  }, []);
+  return (
+    <Layout>
+      <Sidebar />
+      <Layout>
+        <Head />
+        <Content>
+          <Outlet />
+        </Content>
+      </Layout>
+    </Layout>
+  );
 }
